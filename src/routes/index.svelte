3d87e8f72539a8e1210e1eb3e00e2d3f31f3bb6b
Ozgur Dincer <ozgurdincer@kentajans.com>
2021-05-15 19:54:07 +0300

    homepage slider images replaces by webp

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -21,18 +21,18 @@
 />
 
 <div id="myScroll" class="flex flex-col justify-start p-1 md:overflow-hidden md:flex-row">
-    <img src="/images/collection/BLK-JEANS_DENIM_DNM_2015_COLLECTION-5.jpg" alt="BLK Jeans Menswear Slide" loading="lazy" />
-    <img src="/images/collection/BLK-JEANS_DENIM_DNM_2015_COLLECTION-9.jpg" alt="BLK Jeans Menswear Slide" loading="lazy" />
-    <img src="/images/collection/BLK-JEANS_DENIM_DNM_2015_COLLECTION-6.jpg" alt="BLK Jeans Menswear Slide" loading="lazy" />
-    <img src="/images/collection/BLK-JEANS_DENIM_DNM_2015_COLLECTION-7.jpg" alt="BLK Jeans Menswear Slide" loading="lazy"/>
-    <img src="/images/collection/BLK-JEANS_DENIM_DNM_2015_COLLECTION-12.jpg" alt="BLK Jeans Menswear Slide" loading="lazy" />
-    <img src="/images/collection/BLK-JEANS_DENIM_DNM_2015_COLLECTION-13.jpg" alt="BLK Jeans Menswear Slide" loading="lazy" />
-    <img src="/images/collection/BLK-JEANS_DENIM_DNM_2015_COLLECTION-21.jpg" alt="BLK Jeans Menswear Slide" loading="lazy" />
-    <img src="/images/collection/BLK-JEANS_DENIM_DNM_2015_COLLECTION-26.jpg" alt="BLK Jeans Menswear Slide" loading="lazy" />
-    <img src="/images/collection/BLK-JEANS_DENIM_DNM_2015_COLLECTION-33.jpg" alt="BLK Jeans Menswear Slide" loading="lazy" />
-    <img src="/images/collection/BLK-JEANS_DENIM_DNM_2015_COLLECTION-36.jpg" alt="BLK Jeans Menswear Slide" loading="lazy" />
-    <img src="/images/collection/BLK-JEANS_DENIM_DNM_2015_COLLECTION-39.jpg" alt="BLK Jeans Menswear Slide" loading="lazy" />
-    <img src="/images/collection/BLK-JEANS_DENIM_DNM_2015_COLLECTION-38.jpg" alt="BLK Jeans Menswear Slide" loading="lazy" />
+    <img src="/images/collection/BLK-JEANS_DENIM_DNM_2015_COLLECTION-5.jpg.webp" alt="BLK Jeans Menswear Slide" loading="lazy" />
+    <img src="/images/collection/BLK-JEANS_DENIM_DNM_2015_COLLECTION-9.jpg.webp" alt="BLK Jeans Menswear Slide" loading="lazy" />
+    <img src="/images/collection/BLK-JEANS_DENIM_DNM_2015_COLLECTION-6.jpg.webp" alt="BLK Jeans Menswear Slide" loading="lazy" />
+    <img src="/images/collection/BLK-JEANS_DENIM_DNM_2015_COLLECTION-7.jpg.webp" alt="BLK Jeans Menswear Slide" loading="lazy"/>
+    <img src="/images/collection/BLK-JEANS_DENIM_DNM_2015_COLLECTION-12.jpg.webp" alt="BLK Jeans Menswear Slide" loading="lazy" />
+    <img src="/images/collection/BLK-JEANS_DENIM_DNM_2015_COLLECTION-13.jpg.webp" alt="BLK Jeans Menswear Slide" loading="lazy" />
+    <img src="/images/collection/BLK-JEANS_DENIM_DNM_2015_COLLECTION-21.jpg.webp" alt="BLK Jeans Menswear Slide" loading="lazy" />
+    <img src="/images/collection/BLK-JEANS_DENIM_DNM_2015_COLLECTION-26.jpg.webp" alt="BLK Jeans Menswear Slide" loading="lazy" />
+    <img src="/images/collection/BLK-JEANS_DENIM_DNM_2015_COLLECTION-33.jpg.webp" alt="BLK Jeans Menswear Slide" loading="lazy" />
+    <img src="/images/collection/BLK-JEANS_DENIM_DNM_2015_COLLECTION-36.jpg.webp" alt="BLK Jeans Menswear Slide" loading="lazy" />
+    <img src="/images/collection/BLK-JEANS_DENIM_DNM_2015_COLLECTION-39.jpg.webp" alt="BLK Jeans Menswear Slide" loading="lazy" />
+    <img src="/images/collection/BLK-JEANS_DENIM_DNM_2015_COLLECTION-38.jpg.webp" alt="BLK Jeans Menswear Slide" loading="lazy" />
 </div>
 <div class="scrollBar hidden md:inline-block">
     <button on:click="{()=>scrollMe(-400,0)}">
